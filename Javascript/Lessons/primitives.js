82cd3a07efd1087c92e55d411b92126c894ee700
Frank V. Adames <frankv.adames@outlook.com>
2017-06-14 23:19:55 -0400

    Javascript Primitive DataTypes NOTES

---
+++ Javascript/Lessons/primitives.js
@@ -0,0 +1,47 @@
+//PRIMITIVE DATATYPES
+
+//Numbers
+4
+9.3
+-10
+
+	//We can do some math
+	4 + 10 //14
+	1/5	//0.2
+
+	//Modulo - remainder operator
+	10 % 3 //1
+	24 % 2 //0
+	15 % 11 //4
+
+//Strings
+"Hello World" //If it is inside of quotes it is a string.
+"43"
+
+	//Single or Double quotes OK
+	"hello world"
+	'hello world'
+
+	//Concatenation
+	"charlie" + "brown" //"charliebrown"
+
+	//Escape Characters start with "\" Allows to add a double quote in the string
+	"Singing \"Do wah diddy, diddy, dum diddy do\" "
+	"This is a backslash: \\"
+
+	//Strings have a length property
+	"hello world".length //11
+
+	//Access individual characters using [] and an index
+	"hello"[0] //"h"
+	"hello"[4] //"o"
+
+
+
+//Booleans
+true
+false
+
+//null and undefined
+null
+undefined

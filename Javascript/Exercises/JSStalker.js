55080ba5ab8225db052304ceb52df52e1f01be7d
Frank V. Adames <frankv.adames@outlook.com>
2017-06-15 00:17:35 -0400

    JS Stalker Exercise DONE

---
+++ Javascript/Exercises/JSStalker.js
@@ -0,0 +1,9 @@
+var userFirstName = prompt("What is your first name?");
+var userLastName = prompt("What is your last name?");
+var fullNameSentence = "Hello, " + userFirstName + " " + userLastName + ". It is nice to meet you."
+alert(fullNameSentence);
+
+var userAge = prompt("What about your age, how old are you?");
+var ageSentence = "Oh, you're " + userAge + " years old? That's how old my daughter is."
+alert(ageSentence);
+
---
+++ Javascript/Lessons/builtinmethods.js
@@ -0,0 +1,14 @@
+//Useful Built-In Methods!
+
+//alert
+alert(passInSomethingToBeAlerted);
+alert("Hello there!");
+alert(19 + 567);
+
+//prompt - Ask user for input
+prompt(giveItSomeTextToPrompt);
+prompt("What is your name?");
+
+
+//console.log - prints something to the JS console
+console.log("Hello from the console!");

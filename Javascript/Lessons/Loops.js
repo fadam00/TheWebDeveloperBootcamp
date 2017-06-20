10ac76946515b190806d9ef2b4d9c098b2d196bd
Frank V. Adames <frankv.adames@outlook.com>
2017-06-19 21:39:45 -0400

    Added For Loops notes to Loop NOTES

--- Javascript/Lessons/Loops.js
+++ Javascript/Lessons/Loops.js
@@ -44,6 +44,26 @@ while(count < str.length) {
 //Infinite loops occur when the terminating condition in a loop is never true
 var count = 0;
 
+//FOR LOOPS
+//Similar to while loops but different syntax
+
+//for (init; condition; step) {
+	//run some code}
+
+//Printing numbers from 1 - 5 using for loops
+for(var count = 1; count < 6; count++){
+	console.log(count);
+}
+
+//Printing each character in a string with a for loop
+var str = "hello";
+for(var i = 0; i < str.length; i++){
+	console.log(str[i]);
+}
+
+
+
+
 while(count < 10){
 	console.log("Infinite loop!")
 	//THIS WILL RUN INDEFINITELY. YOU MUST ADD AN INCREMENT/DECREMENT TO MAKE STATEMENT FALSE TO BREAK LOO
--- Javascript/Lessons/loopsPS.js
+++ Javascript/Lessons/loopsPS.js
@@ -8,6 +8,13 @@ while(count < 20){
 	count ++;
 }
 
+//For Loop Version
+
+for(var i = -10; i <= 19; i++){
+	console.log(i);
+}
+
+
 //Print all even numbers between 10 and 40
 
 var count = 10;
@@ -16,6 +23,13 @@ while (count <= 40){
 	console.log(count);
 	count += 2;
 }
+
+//For Loop Version
+
+for(var i = 10; i <= 40; i += 2){
+	console.log(i);
+}
+
 
 //Print all odd numbers between 300 and 333;
 
@@ -28,6 +42,13 @@ while(count <= 333){
 	count++;
 }
 
+//For Loop Version
+
+for(var i = 300; i <= 333; i++){
+	if(i % 2 === 1){
+		console.log(i);
+	}
+}
 //Print all numbers divisible by 5 AND 3 between 5 and 50
 var count = 5;
 
@@ -37,4 +58,11 @@ while(count <= 50){
 	}
 
 	count++;
+}
+
+//For Loop Version
+for(var i = 5; i <= 50; i++){
+	if(i % 3 === 0 && i % 5 === 0){
+		console.log(i);
+	}
 }

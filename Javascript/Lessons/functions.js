c7c0e2bf47974ef8fc7d3f210184fdea3af1ce28
Frank V. Adames <frankv.adames@outlook.com>
2017-06-20 11:03:21 -0400

    Arguments NOTES

--- Javascript/Lessons/functions.js
+++ Javascript/Lessons/functions.js
@@ -7,3 +7,27 @@
 
 //CALL THE FUNCTION
 //doSomething();
+
+function singSong(){
+	console.log("All the small things");
+	console.log("True care, truth brings");
+	console.log("I'll take, one left, your right, best trip");
+}
+
+singSong();
+
+//ARGUMENTS
+//Often we want to write functions that take inputs
+function square(num){
+	console.log(num * num);
+}
+
+square(10); //prints 100
+square(3); //prints 9
+square(4); //prints 16
+
+function sayHello(name){
+	console.log("Hello there, " + name + "!");
+}
+
+sayHello("Frank")

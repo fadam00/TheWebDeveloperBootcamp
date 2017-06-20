63a5f124bf89565c5e1c9c2873f3a25d61e5ecb0
Frank V. Adames <frankv.adames@outlook.com>
2017-06-19 22:30:37 -0400

    Functions NOTES pt.1

---
+++ Javascript/Lessons/functions.js
@@ -0,0 +1,9 @@
+//FUNCTIONS
+//Functions are like variables for BLOCKS OF CODE. They're reusable bits of code
+
+//DECLARING A FUNCTION
+//function doSomething(){
+	//console.log("Hello World!");}
+
+//CALL THE FUNCTION
+//doSomething();

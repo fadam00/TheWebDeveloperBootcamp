1498a3c2d55252bdee01e39a44d2b56998754e2f
Frank V. Adames <frankv.adames@outlook.com>
2017-06-29 13:28:34 -0400

    Refactored isEven and new version for factorial functions

--- Javascript/Exercises/functionsPS.js
+++ Javascript/Exercises/functionsPS.js
@@ -4,22 +4,23 @@
 
 function isEven(num){
 	if(num % 2 === 0 ){
-		console.log(true);
+		//return true if even
+		return true;
 	}else {
-		console.log(false);
+		//return false if not
+		return false;
 	}
 };
 
+//Refactored Version
+
+function isEven(num){
+	return (num % 2 === 0)
+}
+
 isEven(4);
 
-// function factorial(num){
-// 	for(var i = num; i > 1; i--){
-// 		console.log(i *= (num - 1));
-// 	}
-// }
 
-
-// factorial(5); //Don't know...
 function factorial(num){
 	if(num === 0){
 		return 1
@@ -29,11 +30,28 @@ function factorial(num){
 			num = num * (i - 1);
 		}
 
-	console.log(num);
+	return num
 
 	}
 }
 
-factorial(0);
+factorial(5);
+
+//Another way to do it
+
+function factorial(num){
+	//define a result variable
+	var result = 1;
+	//calculate factorial and store value in result
+	for(var i = 1; i <= num; i++){
+		result *= i;
+	}
+
+	return result;
+
+}
+
+factorial(5)
+
 
 

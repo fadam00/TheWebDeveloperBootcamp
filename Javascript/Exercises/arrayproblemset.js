976025d62d2883e11be46e859069ed40cb1d88c0
Frank V. Adames <frankv.adames@outlook.com>
2017-09-19 21:02:54 -0400

    Array problem set complete

---
+++ Javascript/Exercises/arrayproblemset.js
@@ -0,0 +1,50 @@
+//printReverse()
+
+function printReverse(array){
+	for(i = array.length; i >= 0; i--){
+		console.log(array[i]);
+	}
+}
+
+printReverse([1,2,3,4,5,6,7,8]);
+
+//isUniform
+
+function isUniform(array){
+		var proof = array[0];
+		var arrProof = [];
+	array.forEach(function(item){
+		if(item === proof){
+			arrProof.push(item);
+		};
+
+	});
+
+	if(array.length === arrProof.length){
+			return true;
+		} else {
+			return false;
+		}
+};
+
+//sumArray
+
+function sumArray(array){
+	var sum = 0;
+	array.forEach(function(item){
+		sum += item;
+	});
+	return sum;
+};
+
+//max()
+
+function max(array){
+	var maxNum = 0;
+	array.forEach(function(item){
+		if(item > maxNum){
+			maxNum = item;
+		};
+	});
+	return maxNum;
+}
